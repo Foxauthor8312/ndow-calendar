8277d181e55d28dc3d04c503f9495d6e68a7e9a5
Foxauthor8312 <lvrcav8tr@gmail.com>
2026-07-09 19:43:10 -0700

    Create standard-event-instructions.js

---
+++ templates/standard-event-instructions.js
@@ -0,0 +1,37 @@
+export default function renderStandardInstructions(){
+
+    return `
+
+<h3>
+
+Before You Arrive
+
+</h3>
+
+<ul>
+
+<li>
+Arrive approximately 30 minutes before the scheduled start time.
+</li>
+
+<li>
+Wear comfortable clothing suitable for both indoor and outdoor activities.
+</li>
+
+<li>
+Bring water and snacks, especially for longer classes.
+</li>
+
+<li>
+A government-issued photo ID may be required.
+</li>
+
+<li>
+If you are unable to attend, please cancel your registration so another participant may attend.
+</li>
+
+</ul>
+
+`;
+
+}
