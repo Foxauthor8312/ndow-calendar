e5188d365a05920c0023ce9082672504fbb9fab1
Foxauthor8312 <lvrcav8tr@gmail.com>
2026-07-09 19:01:49 -0700

    Create survey template with header and footer
    Added a survey template with email header and footer.

--- templates/survey-template.js
+++ templates/survey-template.js
@@ -1,1 +1,60 @@
+import renderEmailHeader
+from './email-header.js';
 
+import renderEmailFooter
+from './email-footer.js';
+
+export default function renderSurveyTemplate(data){
+
+    return `
+
+${renderEmailHeader()}
+
+<h2>
+
+Thank You for Attending
+
+</h2>
+
+<p>
+
+Thank you for attending
+
+<strong>
+
+${data.eventTitle}
+
+</strong>.
+
+</p>
+
+<p>
+
+Your feedback helps the Nevada Department of Wildlife improve future programs.
+
+</p>
+
+<p>
+
+Please take about one minute to complete our survey.
+
+</p>
+
+<div style="text-align:center;margin:30px 0;">
+
+<a
+    href="${data.surveyUrl}"
+    class="email-button"
+>
+
+Take Survey
+
+</a>
+
+</div>
+
+${renderEmailFooter()}
+
+`;
+
+}
