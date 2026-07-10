acad64c14563a407c80fb0ac5c007b63088fe833
Foxauthor8312 <lvrcav8tr@gmail.com>
2026-07-10 14:49:40 -0700

    Update login-background.js

--- js/login/login-background.js
+++ js/login/login-background.js
@@ -7,15 +7,18 @@ Login Background
 
 const LoginBackground = {
 
+    currentImage: 0,
+
     initialize(){
 
-        this.loadImage(1);
+        this.showRandomImage();
 
     },
 
-    loadImage(number){
+    showRandomImage(){
 
         const background =
+
             document.getElementById(
                 'loginBackground'
             );
@@ -23,14 +26,53 @@ const LoginBackground = {
         if(!background)
             return;
 
-        background.style.backgroundImage =
-            `url(images/login/image-${number}.webp)`;
+        let image;
 
-        requestAnimationFrame(() => {
+        do{
+
+            image =
+
+                Math.floor(
+
+                    Math.random() *
+
+                    LoginConfig.imageCount
+
+                ) + 1;
+
+        }
+
+        while(
+
+            LoginConfig.preventRepeat &&
+
+            image ===
+
+            LoginStorage.getLastImage()
+
+        );
+
+        LoginStorage.setLastImage(
+
+            image
+
+        );
+
+        this.currentImage = image;
+
+        background.style.opacity = 0;
+
+        setTimeout(() => {
+
+            background.style.backgroundImage =
+
+                `url(${LoginConfig.imagePath}image-${image}.${LoginConfig.imageExtension})`;
 
             background.style.opacity = 1;
 
-        });
+        },
+
+        LoginConfig.fadeDuration / 2);
 
     }
 
