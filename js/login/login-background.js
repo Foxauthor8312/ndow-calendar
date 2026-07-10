7220e1841b49f8cf4848b366fda5d50a2ec2df67
Foxauthor8312 <lvrcav8tr@gmail.com>
2026-07-10 13:36:43 -0700

    Implement LoginBackground object for login screen
    Added a LoginBackground object with methods to initialize and load a background image for the login screen.

--- js/login/login-background.js
+++ js/login/login-background.js
@@ -1,1 +1,37 @@
+/*
+==========================================================
+NDOW Volunteer Portal
+Login Background
+==========================================================
+*/
 
+const LoginBackground = {
+
+    initialize(){
+
+        this.loadImage(1);
+
+    },
+
+    loadImage(number){
+
+        const background =
+            document.getElementById(
+                'loginBackground'
+            );
+
+        if(!background)
+            return;
+
+        background.style.backgroundImage =
+            `url(images/login/image-${number}.webp)`;
+
+        requestAnimationFrame(() => {
+
+            background.style.opacity = 1;
+
+        });
+
+    }
+
+};
