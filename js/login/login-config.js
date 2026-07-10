78614ffda5c02f5813b1a6e495109d03335cdd45
Foxauthor8312 <lvrcav8tr@gmail.com>
2026-07-10 12:26:45 -0700

    Update login-config.js

--- js/login/login-config.js
+++ js/login/login-config.js
@@ -1,1 +1,19 @@
+const LoginConfig = {
 
+    imagePath: 'images/login/',
+
+    imageCount: 11,
+
+    imageExtension: 'webp',
+
+    panelPosition: 'right',
+
+    panelOpacity: .72,
+
+    overlayOpacity: .20,
+
+    fadeDuration: 800,
+
+    preventRepeat: true
+
+};
