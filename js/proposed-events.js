a77f678e81c531b8832910aa2d3d52951d00be2f
Foxauthor8312 <lvrcav8tr@gmail.com>
2026-09-22 09:42:39 -0700

    Change date format and style in proposed-events.js
    Updated date display format and styling in proposed-events.js.

--- js/proposed-events.js
+++ js/proposed-events.js
@@ -315,15 +315,22 @@ async function renderProposedEvents() {
           0 1px 2px rgba(0,0,0,.04);
       ">
 
-        <div style="
-          color:#19304B;
-          font-size:20px;
-          font-weight:700;
-          line-height:1;
-          margin-bottom:5px;
-        ">
-          ${date.getDate()}
-        </div>
+  <div style="
+  color:#19304B;
+  font-size:13px;
+  font-weight:700;
+  line-height:1.2;
+  margin-bottom:5px;
+">
+  ${date.toLocaleDateString(
+    'en-US',
+    {
+      month:'long',
+      day:'numeric',
+      year:'numeric'
+    }
+  )}
+</div>
 
         <div style="
           color:#19304B;
